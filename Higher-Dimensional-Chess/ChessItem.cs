1af55c2507679ca5985257c402d30307f0079a79
Sofviic <sofviic1993@gmail.com>
2020-11-14 13:08:05 +0300

    Cleaned comments.

--- Higher-Dimensional-Chess/ChessItem.cs
+++ Higher-Dimensional-Chess/ChessItem.cs
@@ -29,71 +29,8 @@ namespace _3DChess {
 			royal = other.royal;
 		}
 		public ChessItem Clone() => new ChessItem(this); 
-
-		//======================================================================================================================================================================OLD-MOVMENT
-		/*
-		public (int, int)[] Movement(int x, int y) {
-			return
-				p == ChessBoard.P.WP || p == ChessBoard.P.BP ? PawnMovement(x, y) :
-				p == ChessBoard.P.WN || p == ChessBoard.P.BN ? KnightMovement(x, y) :
-				p == ChessBoard.P.WK || p == ChessBoard.P.BK ? KingMovement(x, y) :
-				p == ChessBoard.P.WB || p == ChessBoard.P.BB ? BishopMovement(x, y) :
-				p == ChessBoard.P.WR || p == ChessBoard.P.BR ? RookMovement(x, y) :
-				p == ChessBoard.P.WQ || p == ChessBoard.P.BQ ? QueenMovement(x, y) :
-				new (int, int)[0];
-		}
-
-		private (int, int)[] PawnMovement(int x, int y) {
-			List<(int, int)> res = new List<(int, int)>();
-			for(int j = 0; j < board.r; ++j) for(int i = 0; i < board.c; ++i)
-					if(hasMoved && PawnOnceMovable(x, y, i, j, pc == ChessBoard.PColour.B) ||
-					 !hasMoved && PawnTwiceMovable(x, y, i, j, pc == ChessBoard.PColour.B)
-						)
-						res.Add((i, j));
-			return res.ToArray();
-		}
-
-		private (int, int)[] KnightMovement(int x, int y) {
-			List<(int, int)> res = new List<(int, int)>();
-			for(int j = 0; j < board.r; ++j) for(int i = 0; i < board.c; ++i)
-					if(KnightMovable(x, y, i, j))
-						res.Add((i, j));
-			return res.ToArray();
-		}
-
-		private (int, int)[] KingMovement(int x, int y) {
-			List<(int, int)> res = new List<(int, int)>();
-			for(int j = 0; j < board.r; ++j) for(int i = 0; i < board.c; ++i)
-					if(KingMovable(x, y, i, j))
-						res.Add((i, j));
-			return res.ToArray();
-		}
-
-		private (int, int)[] BishopMovement(int x, int y) {
-			List<(int, int)> res = new List<(int, int)>();
-			for(int j = 0; j < board.r; ++j) for(int i = 0; i < board.c; ++i)
-					if(BishopMovable(x, y, i, j))
-						res.Add((i, j));
-			return res.ToArray();
-		}
-
-		private (int, int)[] RookMovement(int x, int y) {
-			List<(int, int)> res = new List<(int, int)>();
-			for(int j = 0; j < board.r; ++j) for(int i = 0; i < board.c; ++i)
-					if(RookMovable(x, y, i, j))
-						res.Add((i, j));
-			return res.ToArray();
-		}
-
-		private (int, int)[] QueenMovement(int x, int y) {
-			List<(int, int)> res = new List<(int, int)>();
-			for(int j = 0; j < board.r; ++j) for(int i = 0; i < board.c; ++i)
-					if(QueenMovable(x, y, i, j))
-						res.Add((i, j));
-			return res.ToArray();
-		}
-		*/
-		//======================================================================================================================================================================NEW-MOVMENT
+		
+		//======================================================================================================================================================================MOVMENT
 		public (int, int)[] Movement(int x, int y) {
 			return
 				p == ChessBoard.P.WP || p == ChessBoard.P.BP ? PawnMovement(x, y) :
@@ -155,27 +92,7 @@ namespace _3DChess {
 			return res.ToArray();
 		}
 
-
-		//======================================================================================================================================================================OLD-MOVMENT-CHECKS
-		/*
-		private static bool PawnOnceMovable(int sx, int sy, int dx, int dy, bool posY) => dx == sx && dy == sy + (posY ? 1 : -1);
-		private static bool PawnTwiceMovable(int sx, int sy, int dx, int dy, bool posY) => dx == sx && (dy == sy + (posY ? 1 : -1) || dy == sy + (posY ? 2 : -2));
-
-		private static bool _21Movable(int sx, int sy, int dx, int dy) => (dx == sx + 2 || dx == sx - 2) && (dy == sy + 1 || dy == sy - 1);
-		private static bool _12Movable(int sx, int sy, int dx, int dy) => (dx == sx + 1 || dx == sx - 1) && (dy == sy + 2 || dy == sy - 2);
-		private static bool KnightMovable(int sx, int sy, int dx, int dy) => _21Movable(sx, sy, dx, dy) || _12Movable(sx, sy, dx, dy);
-
-		private static bool _1DiagMovable(int sx, int sy, int dx, int dy) => (dx == sx + 1 || dx == sx - 1) && (dy == sy + 1 || dy == sy - 1);
-		private static bool _1OrthMovable(int sx, int sy, int dx, int dy) => (dx == sx && (dy == sy + 1 || dy == sy - 1)) || ((dx == sx + 1 || dx == sx - 1) && dy == sy);
-		private static bool KingMovable(int sx, int sy, int dx, int dy) => _1DiagMovable(sx, sy, dx, dy) || _1OrthMovable(sx, sy, dx, dy);
-
-		private static bool BishopMovable(int sx, int sy, int dx, int dy) => dx != sx && Math.Abs(dx - sx) == Math.Abs(dy - sy);
-
-		private static bool RookMovable(int sx, int sy, int dx, int dy) => dx == sx ^ dy == sy;
-
-		private static bool QueenMovable(int sx, int sy, int dx, int dy) => BishopMovable(sx, sy, dx, dy) || RookMovable(sx, sy, dx, dy);
-		*/
-		//======================================================================================================================================================================NEW-MOVMENT-CHECKS
+		//======================================================================================================================================================================MOVMENT-CHECKS
 		private static bool Movable((int, int) m, (int, int) p, (int, int) t) => t == p.Add(m);
 		private static bool Movable(IEnumerable<(int, int)> m, (int, int) p, (int, int) t) => m.Select(x => x.Add(p)).Any(x => x == t);
 		private static bool MovableRepeat(ChessBoard board, int n, (int, int) m, (int, int) p, (int, int) t) => n < 1 ? false : Movable(m, p, t) || MovableRepeat(board, n - 1, m, p, t.Sub(m));
