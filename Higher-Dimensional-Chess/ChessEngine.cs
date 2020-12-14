4acd01184e462bb526675acf5000f537ea10904f
Sofviic <sofviic1993@gmail.com>
2020-12-14 17:44:00 +0300

    Added a new ChessEngineClass.

---
+++ Higher-Dimensional-Chess/ChessEngine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3DChess {
+	class ChessEngine {
+		Vector2 boardSize;
+		public ChessEngine(Vector2 boardSize) => this.boardSize = boardSize;
+
+	}
+}
